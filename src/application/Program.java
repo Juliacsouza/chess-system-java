9680885fef31dabe63e06de3cbdda6d093e0ed68
Julia Carvalho <juliacsouza573@gmail.com>
2024-04-01 22:26:51 -0300

    Fist class: position

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,13 +3,16 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("HelloWorld!");
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 		
 		sc.close();
 
